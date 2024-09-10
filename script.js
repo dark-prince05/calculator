dd4f4017ff005996923f8700014caea2ee5cf852
Dark-prince <barathraj0504@gmail.com>
2024-09-10 19:15:20 +0530

    Added an operate function that takes two numbers and an operator, calculates, and returns the value.

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ let multiplication = (a, b) => {
 let division = (a, b) => {
   return a / b;
 };
+
+let firstNum = 50;
+let operator = "/";
+let secondNum = 34;
+
+let operate = (num1, operator, num2) => {
+  let res;
+  switch (operator) {
+    case "+":
+      res = addition(num1, num2);
+      break;
+    case "-":
+      res = subtraction(num1, num2);
+      break;
+    case "*":
+      res = multiplication(num1, num2);
+      break;
+    case "/":
+      res = division(num1, num2);
+      break;
+  }
+  return res;
+};
+
+console.log(operate(firstNum, operator, secondNum));
