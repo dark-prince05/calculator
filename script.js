c4e14c50a5d15cbeb6c3b73fabd32010ceded2c8
Dark-prince <barathraj0504@gmail.com>
2024-09-10 18:57:13 +0530

    add four function add,sub,mul and div

---
+++ script.js
@@ -0,0 +1,15 @@
+let addition = (a, b) => {
+  return a + b;
+};
+
+let subtraction = (a, b) => {
+  return a - b;
+};
+
+let multiplication = (a, b) => {
+  return a * b;
+};
+
+let division = (a, b) => {
+  return a / b;
+};
