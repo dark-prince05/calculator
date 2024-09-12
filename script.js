051dd06f2f912f4cb86ef9af4124efb25256916a
Dark-prince <barathraj0504@gmail.com>
2024-09-12 15:31:44 +0530

    Added functions for button clicks to perform calculations

--- script.js
+++ script.js
@@ -1,45 +1,80 @@
-let addition = (a, b) => {
-  return a + b;
-};
+let addition = (a, b) => a + b;
 
-let subtraction = (a, b) => {
-  return a - b;
-};
+let subtraction = (a, b) => a - b;
 
-let multiplication = (a, b) => {
-  return a * b;
-};
+let multiplication = (a, b) => a * b;
 
-let division = (a, b) => {
-  return a / b;
-};
+let division = (a, b) => a / b;
 
-let modulus = (a, b) => {
-  return a % b;
-};
-
-let firstNum = 50;
-let operator = "/";
-let secondNum = 34;
+let modulus = (a, b) => a % b;
 
 let operate = (num1, operator, num2) => {
-  let res;
   switch (operator) {
     case "+":
-      res = addition(num1, num2);
-      break;
+      return addition(num1, num2);
     case "-":
-      res = subtraction(num1, num2);
-      break;
+      return subtraction(num1, num2);
     case "*":
-      res = multiplication(num1, num2);
-      break;
+      return multiplication(num1, num2);
     case "/":
-      res = division(num1, num2);
-      break;
+      if (num2 === 0) {
+        return "hmm..trying to divide by 0";
+      } else {
+        return division(num1, num2).toFixed(5);
+      }
     case "%":
-      res = modulus(num1, num2);
-      break;
+      return modulus(num1, num2);
   }
-  return res;
 };
+
+let firstNum = 0;
+let operator = "";
+let secondNum = 0;
+let content = false;
+
+const display = document.querySelector("#result-display");
+const numBtns = document.querySelectorAll(".num-btn");
+const opBtns = document.querySelectorAll(".op-btn");
+const equals = document.querySelector("#equals");
+
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (content) {
+      display.textContent = "";
+      content = false;
+    }
+    display.textContent += e.target.id;
+  });
+});
+
+opBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.id === "clear") {
+      display.textContent = "";
+      firstNum = 0;
+      secondNum = 0;
+      operator = "";
+      content = false;
+    } else if (e.target.id === "erase") {
+      display.textContent = display.textContent.slice(0, -1);
+    } else if (operator !== "") {
+      secondNum = +display.textContent;
+      display.textContent = operate(firstNum, operator, secondNum);
+      firstNum = +display.textContent;
+      operator = e.target.id;
+      content = true;
+    } else {
+      firstNum = +display.textContent;
+      operator = e.target.id;
+      content = true;
+    }
+  });
+});
+
+equals.addEventListener("click", () => {
+  secondNum = +display.textContent;
+  if (operator !== "") {
+    display.textContent = operate(firstNum, operator, secondNum);
+  }
+  operator = "";
+});
